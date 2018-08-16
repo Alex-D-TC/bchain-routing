1b85538c6dd1e22d5e36676190c5299b6a56a6a9
Alexandru Duma <alexandru.duma@spyhce.com>
2018-08-16 15:48:33 +0300

    Implemented client node start

--- cmd/client.go
+++ cmd/client.go
@@ -1,9 +1,43 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
 
-var clientCmd = &cobra.Command{}
+	"github.com/alex-d-tc/bchain-routing/swiss"
+	"github.com/spf13/cobra"
+)
+
+var clientCmd = &cobra.Command{
+	Use:   "client-start",
+	Short: "Start a client node",
+	Long:  "Starrrrt a cliennnnt nooooode",
+	Run: func(cmd *cobra.Command, args []string) {
+		runClient(*clientLocalIPP, *clientGlobalIPP, int(*clientPortP), *clientBootstrapIPP, int(*clientBootstrapPortP))
+	},
+}
+
+var clientPortP *int32
+var clientLocalIPP *string
+var clientGlobalIPP *string
+var clientBootstrapIPP *string
+var clientBootstrapPortP *int32
 
 func init() {
+	flags := clientCmd.Flags()
+
+	flags.Int32Var(clientPortP, "port", 8080, "The client port")
+	flags.StringVar(clientLocalIPP, "local-ip", "127.0.0.1", "The local ip")
+	flags.StringVar(clientGlobalIPP, "global-ip", "0.0.0.0", "The global ip")
+	flags.StringVar(clientBootstrapIPP, "bootstrap-ip", "127.0.0.1", "The bootstrap node ip")
+	flags.Int32Var(clientBootstrapPortP, "bootstrap-port", 8000, "The client bootstrap port")
+
 	rootCmd.AddCommand(clientCmd)
+}
+
+func runClient(localIP string, publicIP string, port int, bootstrapIP string, bootstrapPort int) {
+	node := swiss.InitSwissNode(localIP, port, publicIP)
+	err := node.JoinAndStart(bootstrapIP, bootstrapPort)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
